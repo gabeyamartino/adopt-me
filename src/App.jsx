ff755f95e6090f4e08e0cfeba86921dc4e5a02ed
gabeyamartino <gabeyamartino@gmail.com>
2022-12-30 13:53:58 -0500

    Created custom useBreedList hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Pet from "./Pet.jsx";
+import SearchParams from "./SearchParams.jsx";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Hazel" animal="cat" breed="tuxedo" />
-      <Pet name="Ochi" animal="dog" breed="mixed" />
-      <Pet name="Roach" animal="horse" breed="mare" />
+      <SearchParams />
     </div>
   );
 };
